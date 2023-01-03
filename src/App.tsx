7b131168bdeafce5cb2648c82a8e1eb4e9130ca7
Iniok <ilidanilo@mail.ru>
2023-01-03 15:02:40 +0300

    Lesson 10

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Technologies from "./Components/Technologies";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <div className="App">
             <Header/>
             <Technologies/>
+            <Footer/>
         </div>
     );
 }
