16cfd2575514277507f5c7546121bd34cc2fe5fd
Iniok <ilidanilo@mail.ru>
2023-01-10 19:41:52 +0300

    Lesson 18

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
+import Dialogs from "./Components/Dialogs/Dialogs";
 
 
 function App() {
     return (
         <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <Profile/>
-                <Footer/>
+            <Header/>
+            <Navbar/>
+
+            <div className="app-wrapper-content">
+                <Dialogs/>
+                {/*<Profile/>*/}
+            </div>
+            <Footer/>
         </div>
     );
 }
