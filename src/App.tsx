6ff2b891b8394b8025b3458cd03d8d34508fe804
Iniok <ilidanilo@mail.ru>
2023-01-10 20:56:35 +0300

    Lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,22 @@ import Header from "./Components/Header/Header";
 import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-
-            <div className="app-wrapper-content">
-                <Dialogs/>
-                {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path="/dialogs" component={Dialogs}/>
+                    <Route path="/profile" component={Profile}/>
+                </div>
+                <Footer/>
             </div>
-            <Footer/>
-        </div>
+        </BrowserRouter>
     );
 }
 
