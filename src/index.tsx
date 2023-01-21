3936be942b5a02762f1bf787fe23e01d130adecf
Iniok <ilidanilo@mail.ru>
2023-01-21 14:45:09 +0300

    Lesson 30

--- src/index.tsx
+++ src/index.tsx
@@ -2,21 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import state from './redux/state';
 
-let dialogsData = [{id: 1, name: 'Dimych'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'}]
 
-let messagesData = [{id: 1, message: 'Hi'},
-    {id: 2, message: 'How are you'},
-    {id: 3, message: 'Yo'},
-    {id: 4, message: 'Yo'},
-    {id: 5, message: 'Yo'},
-    {id: 6, message: 'Yo'}]
-
-let postsData = [{id: 1, message: 'Hi, how are you?', likeCount: 15},
-    {id: 2, message: 'Its my first post', likeCount: 20}]
 
 
 
@@ -24,9 +12,9 @@ let postsData = [{id: 1, message: 'Hi, how are you?', likeCount: 15},
 
 ReactDOM.render(
     <App
-        postsData={postsData}
-        messageData={messagesData}
-        dialogsData={dialogsData}
+        postsData={state.profilePage.postsData}
+        messageData={state.dialogsPage.messagesData}
+        dialogsData={state.dialogsPage.dialogsData}
     />,
     document.getElementById('root')
 );
---
+++ src/redux/state.tsx
@@ -0,0 +1,20 @@
+let state = {
+    profilePage: {
+        postsData: [{id: 1, message: 'Hi, how are you?', likeCount: 15},
+            {id: 2, message: 'Its my first post', likeCount: 20}]
+    },
+    dialogsPage: {
+        dialogsData: [{id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'}],
+        messagesData: [{id: 1, message: 'Hi'},
+            {id: 2, message: 'How are you'},
+            {id: 3, message: 'Yo'},
+            {id: 4, message: 'Yo'},
+            {id: 5, message: 'Yo'},
+            {id: 6, message: 'Yo'}],
+    }
+}
+
+export default state
